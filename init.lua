807ca27fe9750630e543fae2d267860def433a0b
getzze <getzze@gmail.com>
2015-10-09 15:23:25 +0100

    Patch resize client when change screen

--- init.lua
+++ init.lua
@@ -80,12 +80,40 @@ local function load_property(name,property)
     end
 end
 
+-- Patch change screen for fullscreen
+local function client_reload_max(c)
+    local c = c or client.focus
+    if not c then return end
+    if c.maximized then
+        --naughty.notify({text="Maximized ! " .. c.name, screen=c.screen})
+        c.maximized = false
+        c.maximized = true
+    else
+        if c.maximized_horizontal then
+            c.maximized_horizontal = false
+            c.maximized_horizontal = true
+        end
+        if c.maximized_vertical then
+            c.maximized_vertical = false
+            c.maximized_vertical = true
+        end
+    end
+    if c.fullscreen then
+        --naughty.notify({text="Fullscreen ! " .. c.name, screen=c.screen})
+        c.fullscreen = false
+        c.fullscreen = true
+    end
+end
+
 --Check all focus policies then change focus (Awesome 3.5.3+)
 function module.focus_client(c,properties)
     local properties = properties or (c_rules.instance[string.lower(c.instance or "N/A")] or {}).properties or (c_rules.class[string.lower(get_class(c))] or {}).properties or {}
     if (((not c.transient_for) or (c.transient_for==capi.client.focus) or (not settings.block_children_focus_stealing)) and (not properties.no_autofocus)) then
         if not awful.util.table.hasitem(c:tags(), awful.tag.selected(c.screen or 1)) and (not prop(c:tags()[1],"no_focus_stealing_in")) then
+            -- Focus tag
             awful.tag.viewonly(c:tags()[1])
+            -- Resize client
+            client_reload_max(c)
         end
         capi.client.focus = c
         c:raise()
